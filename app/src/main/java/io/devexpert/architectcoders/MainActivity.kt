109d56424ed1f8c78f2a703b5139203ec4753287
antoniolg <antonio@devexperto.com>
2024-04-10 17:04:31 +0200

    Implement navigation

--- app/src/main/java/io/devexpert/architectcoders/MainActivity.kt
+++ app/src/main/java/io/devexpert/architectcoders/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import io.devexpert.architectcoders.ui.screens.home.HomeScreen
+import io.devexpert.architectcoders.ui.screens.Navigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            HomeScreen()
+            Navigation()
         }
     }
 }
