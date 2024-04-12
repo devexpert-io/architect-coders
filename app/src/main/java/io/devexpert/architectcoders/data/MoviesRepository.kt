94da7b657a2e163a03c171ac6fa26e5446e18a25
antoniolg <antonio@devexperto.com>
2024-04-12 13:52:28 +0200

    Use MoviesService for Home

--- app/src/main/java/io/devexpert/architectcoders/data/MoviesRepository.kt
+++ app/src/main/java/io/devexpert/architectcoders/data/MoviesRepository.kt
@@ -1,12 +1,18 @@
 package io.devexpert.architectcoders.data
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.withContext
-
 class MoviesRepository {
-    suspend fun fetchPopularMovies(): List<Movie> = withContext(Dispatchers.IO) {
-        delay(1000)
-        movies
-    }
+
+    suspend fun fetchPopularMovies(region: String): List<Movie> =
+        MoviesClient.instance.fetchPopularMovies(region)
+            .results
+            .map { it.toDomainModel() }
+
+    suspend fun findMovieById(id: Int): Movie =
+        MoviesClient.instance.fetchMovieById(id).toDomainModel()
 }
+
+private fun RemoteMovie.toDomainModel() = Movie(
+    id = id,
+    title = title,
+    poster = "https://image.tmdb.org/t/p/w185/$posterPath",
+)
