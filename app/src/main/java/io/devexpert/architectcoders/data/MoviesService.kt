2382afe372d23bfb79761bba14d1bdbaca931167
antoniolg <antonio@devexperto.com>
2024-04-12 13:51:43 +0200

    Implement MoviesService

--- app/src/main/java/io/devexpert/architectcoders/data/MoviesService.kt
+++ app/src/main/java/io/devexpert/architectcoders/data/MoviesService.kt
@@ -1,4 +1,15 @@
 package io.devexpert.architectcoders.data
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface MoviesService {
+
+    @GET("discover/movie?sort_by=popularity.desc")
+    suspend fun fetchPopularMovies(@Query("region") region: String): RemoteResult
+
+    @GET("movie/{id}")
+    suspend fun fetchMovieById(@Path("id") id: Int): RemoteMovie
+
 }
