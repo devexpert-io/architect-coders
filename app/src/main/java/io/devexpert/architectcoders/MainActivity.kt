ea73ef972085d5c787a7d1871c64d08db4664673
antoniolg <antonio@devexperto.com>
2024-05-01 13:23:30 +0200

    Model Navigation

--- app/src/main/java/io/devexpert/architectcoders/MainActivity.kt
+++ app/src/main/java/io/devexpert/architectcoders/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import io.devexpert.architectcoders.ui.screens.Navigation
+import io.devexpert.architectcoders.ui.navigation.Navigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
