9d53f3c90050fb080158fd97647b7806f26b9480
antoniolg <antonio@devexperto.com>
2024-05-01 12:51:01 +0200

    Use StateFlow in ViewModels

--- app/src/main/java/io/devexpert/architectcoders/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/io/devexpert/architectcoders/ui/screens/home/HomeViewModel.kt
@@ -1,25 +1,25 @@
 package io.devexpert.architectcoders.ui.screens.home
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.devexpert.architectcoders.data.Movie
 import io.devexpert.architectcoders.data.MoviesRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
     private val repository: MoviesRepository = MoviesRepository()
 
-    var state by mutableStateOf(UiState())
-        private set
+    private val _state = MutableStateFlow(UiState())
+    val state: StateFlow<UiState> = _state.asStateFlow()
 
     fun onUiReady(region: String) {
         viewModelScope.launch {
-            state = UiState(loading = true)
-            state = UiState(loading = false, movies = repository.fetchPopularMovies(region))
+            _state.value = UiState(loading = true)
+            _state.value = UiState(loading = false, movies = repository.fetchPopularMovies(region))
         }
     }
 
