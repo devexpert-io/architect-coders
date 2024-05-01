eaeea8b371584f5ff4c92fc72ba9911e4fb8c46c
antoniolg <antonio@devexperto.com>
2024-05-01 15:21:44 +0200

    UiEvent using Channel

--- app/src/main/java/io/devexpert/architectcoders/ui/screens/detail/DetailViewModel.kt
+++ app/src/main/java/io/devexpert/architectcoders/ui/screens/detail/DetailViewModel.kt
@@ -4,13 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.devexpert.architectcoders.data.Movie
 import io.devexpert.architectcoders.data.MoviesRepository
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 class DetailViewModel(private val id: Int) : ViewModel() {
-
     private val repository: MoviesRepository = MoviesRepository()
 
     private val _state = MutableStateFlow(UiState())
@@ -21,10 +22,21 @@ class DetailViewModel(private val id: Int) : ViewModel() {
         val movie: Movie? = null
     )
 
+    sealed interface UiEvent {
+        data class ShowMessage(val message: String) : UiEvent
+    }
+
+    private val _events = Channel<UiEvent>()
+    val events = _events.receiveAsFlow()
+
     init {
         viewModelScope.launch {
             _state.value = UiState(loading = true)
             _state.value = UiState(loading = false, movie = repository.findMovieById(id))
         }
+    }
+
+    fun onFavoriteClicked() {
+            _events.trySend(UiEvent.ShowMessage("Favorite clicked"))
     }
 }
