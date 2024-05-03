b46f4e43f73037d12807c174511763912d1263d5
antoniolg <antonio@devexperto.com>
2024-05-03 17:49:28 +0200

    Add favorite feature

--- app/src/main/java/io/devexpert/architectcoders/ui/screens/detail/DetailState.kt
+++ app/src/main/java/io/devexpert/architectcoders/ui/screens/detail/DetailState.kt
@@ -5,25 +5,13 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 
 @OptIn(ExperimentalMaterial3Api::class)
 class DetailState(
     val scrollBehavior: TopAppBarScrollBehavior,
     val snackbarHostState: SnackbarHostState
-) {
-    @Composable
-    fun ShowMessageEffect(message: String?, onMessageShown: () -> Unit) {
-        LaunchedEffect(message) {
-            message?.let {
-                snackbarHostState.currentSnackbarData?.dismiss()
-                snackbarHostState.showSnackbar(it)
-                onMessageShown()
-            }
-        }
-    }
-}
+)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
