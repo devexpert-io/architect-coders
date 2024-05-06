b8993fab15cdb728c92589b72ade47f1079606fb
antoniolg <antonio@devexperto.com>
2024-05-06 12:30:10 +0200

    Single Source of Truth

--- app/src/main/java/io/devexpert/architectcoders/data/MoviesRepository.kt
+++ app/src/main/java/io/devexpert/architectcoders/data/MoviesRepository.kt
@@ -3,27 +3,29 @@ package io.devexpert.architectcoders.data
 import io.devexpert.architectcoders.data.datasource.MoviesLocalDataSource
 import io.devexpert.architectcoders.data.datasource.MoviesRemoteDataSource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.onEach
 
 class MoviesRepository(
     private val regionRepository: RegionRepository,
     private val localDataSource: MoviesLocalDataSource,
     private val remoteDataSource: MoviesRemoteDataSource
 ) {
-    val movies: Flow<List<Movie>> = localDataSource.movies.transform { localMovies ->
-        val movies = localMovies.takeIf { it.isNotEmpty() }
-            ?: remoteDataSource.fetchPopularMovies(regionRepository.findLastRegion()).also {
-                localDataSource.save(it)
-            }
-        emit(movies)
+    val movies: Flow<List<Movie>> = localDataSource.movies.onEach { localMovies ->
+        if (localMovies.isEmpty()) {
+            val remoteMoves = remoteDataSource.fetchPopularMovies(regionRepository.findLastRegion())
+            localDataSource.save(remoteMoves)
+        }
     }
 
-    fun findMovieById(id: Int): Flow<Movie> =
-        localDataSource.findMovieById(id).transform { localMovie ->
-            val movie = localMovie
-                ?: remoteDataSource.findMovieById(id).also { localDataSource.save(listOf(it)) }
-            emit(movie)
+    fun findMovieById(id: Int): Flow<Movie> = localDataSource.findMovieById(id)
+        .onEach { movie ->
+            if (movie == null) {
+                val remoteMovie = remoteDataSource.findMovieById(id)
+                localDataSource.save(listOf(remoteMovie))
+            }
         }
+        .filterNotNull()
 
     suspend fun toggleFavorite(movie: Movie) {
         localDataSource.save(listOf(movie.copy(isFavorite = !movie.isFavorite)))
