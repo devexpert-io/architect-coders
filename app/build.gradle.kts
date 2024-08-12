4d83c0a6914fa71f23a9abb2d68db461c6315083
Juanje Cilla <juanje.cilla@gmail.com>
2024-08-12 13:02:35 +0200

    Test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,11 +21,7 @@ android {
             useSupportLibrary = true
         }
 
-        val properties = Properties()
-        properties.load(project.rootProject.file("local.properties").readText().byteInputStream())
-
-        val tmdbApiKey = properties.getProperty("TMDB_API_KEY", "")
-        buildConfigField("String", "TMDB_API_KEY", "\"$tmdbApiKey\"")
+        buildConfigField("String", "TMDB_API_KEY", "\"tests\"")
     }
 
     buildTypes {
